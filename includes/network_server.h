fc12ef62f9636f9171e9521cacb9069b96de4077
Anu-Aravindan <anuraag6@illinois.edu>
2025-04-29 20:40:45 -0500

    network-sever

--- includes/network_server.h
+++ includes/network_server.h
@@ -1,44 +1,44 @@
+// network_server.h
 #pragma once
 
-#include <string>
-#include <memory>
+#include <array>
+#include <cstddef>
 #include <boost/asio.hpp>
+#include "exchange.h"
 
-constexpr unsigned int BUFFER_SIZE = 4096;
-class Exchange;
+using boost::asio::ip::tcp;
+
+// make this available in both .h and .cpp
+static constexpr std::size_t FULL_MSG_LEN =
+    sizeof(uint64_t) + 1 + ORDER_ID_LEN + TICKER_LEN + 4 + 4;
 
 class NetworkServer {
 public:
+    NetworkServer(boost::asio::io_context& io_ctx,
+                  Exchange* exchange,
+                  unsigned short port);
 
-   NetworkServer(boost::asio::io_context& io_ctx, Exchange* exchange, unsigned short port);
-
-   void start();
-
-   void stop();
-
-private:
-
-   void do_accept();
-
-   class Session : public std::enable_shared_from_this<Session> {
-   public:
-
-      Session(boost::asio::ip::tcp::socket socket, Exchange* exchange);
-
-      void start_reading();
-   
-   private:
-      
-      void do_read();
-
-      boost::asio::ip::tcp::socket socket_;
-      Exchange* exchange_;
-      std::array<uint8_t, BUFFER_SIZE> buffer_;
-   };
+    void start();
+    void stop();
 
 private:
-   boost::asio::io_context& io_context_;
-   boost::asio::ip::tcp::acceptor acceptor_;
-   Exchange* exchange_;
-   bool running_;
+    void do_accept();
+
+    boost::asio::io_context&     io_context_;
+    tcp::acceptor                acceptor_;
+    Exchange*                    exchange_;
+    bool                         running_;
+
+    class Session
+      : public std::enable_shared_from_this<Session>
+    {
+    public:
+        Session(tcp::socket socket, Exchange* exchange);
+        void start_reading();
+
+    private:
+        tcp::socket                       socket_;
+        Exchange*                         exchange_;
+        std::array<char, FULL_MSG_LEN>    buffer_;
+    };
 };
