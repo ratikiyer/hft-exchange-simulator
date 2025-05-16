687105f5c0a4d7278d8618b578293d38ed7a7394
ratikiyer <ratikiyer@gmail.com>
2025-05-16 04:11:55 +0000

    added OS specific includes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 // main.cpp
 #include <iostream>
 #include <thread>
-#ifdef __APPLE__
-  #include <libkern/OSByteOrder.h>
-  #define htobe64(x) OSSwapHostToBigInt64(x)
-#else
-  #include <endian.h>
-#endif
 
 #include <boost/asio.hpp>
 #include "exchange.h"
@@ -16,21 +10,13 @@
 #include "network_server.h"
 #include "types.h"
 
+#ifdef __APPLE__
+  #include <libkern/OSByteOrder.h>
+  #define htobe64(x) OSSwapHostToBigInt64(x)
+#else
+  #include <endian.h>
+#endif
+
 int main() {
-    // boost::asio::io_context io_ctx;
-    // logger log("exchange.log");
-    // OrderParser parser;
-    // MarketDataPublisher publisher(io_ctx, "239.255.0.1", 15000);
-    // Exchange ex(&log, &parser, &publisher);
-
-    // ex.start();                     // start threads
-    // ex.add_symbol("ABCD");
-    // ex.add_symbol("WXYZ");
-
-    // NetworkServer server(io_ctx, &ex, 12345);
-    // server.start();
-
-    // std::cout << "Exchange listening on 0.0.0.0:12345\n";
-    // io_ctx.run();
-    return 0;
+  return 0;
 }
